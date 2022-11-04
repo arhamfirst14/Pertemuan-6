278fa69a7811055b3dcec10902862f8e83639341
Arham <92289936+arhamfirst14@users.noreply.github.com>
2022-11-04 18:09:24 +0700

    Add files via upload

---
+++ BMW.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritanche;
+
+/**
+ *
+ * @author LABKOM
+ */
+public class BMW extends Kendaraan{
+
+    public void tampilkan() {
+        input("Mobile", "M3 GTR", 23000);
+        tampilData();
+    }
+
+}
